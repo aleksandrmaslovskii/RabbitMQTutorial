b0d5add81fc24e769e2067edc8e32d21687aa479
Aleksandr Maslovskii <aleksandr.maslovskii.1990@gmail.com>
2022-12-09 20:47:50 +0300

    Publish and Subscribe

---
+++ Receive3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory() {Endpoint = new AmqpTcpEndpoint("localhost", 49154)};
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.ExchangeDeclare(exchange: "logs", type: ExchangeType.Fanout);
+
+var queueName = channel.QueueDeclare().QueueName;
+channel.QueueBind(queue: queueName,
+    exchange: "logs",
+    routingKey: "");
+
+Console.WriteLine(" [*] Waiting for logs.");
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine(" [x] {0}", message);
+};
+channel.BasicConsume(queue: queueName,
+    autoAck: true,
+    consumer: consumer);
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
---
+++ Send3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory() {Endpoint = new AmqpTcpEndpoint("localhost", 49154)};
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.ExchangeDeclare(exchange: "logs", type: ExchangeType.Fanout);
+
+var message = GetMessage(args);
+
+var body = Encoding.UTF8.GetBytes(message);
+channel.BasicPublish(exchange: "logs",
+    routingKey: "",
+    basicProperties: null,
+    body: body);
+
+Console.WriteLine(" [x] Sent {0}", message);
+
+static string GetMessage(string[] args)
+{
+    return args.Length > 0 ? string.Join(" ", args) : "Hello World!";
+}
